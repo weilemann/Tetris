5765bfc87b680c84c4b2784cbb75f620304ce1bb
Yuri Weilemann <yuri_weilemann@hotmail.com>
2021-01-07 11:14:06 -0300

    Created usePlayer and useStage hooks functionalities

--- src/components/Tetris/index.js
+++ src/components/Tetris/index.js
@@ -1,26 +1,37 @@
-import React from 'react';
-
-import {createStage} from '../../gameHelpers';
+import React, { useState } from 'react';
 
 import { StyledTetrisWrapper, StyledTetris } from './styles';
+
+import { usePlayer } from '../../hooks/usePlayer';
+import { useStage } from '../../hooks/useStage';
 
 import Stage from '../Stage';
 import Display from '../Display';
 import StartButton from '../StartButton';
 
 const Tetris = () => {
+    const [dropTime, setDropTime] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
 
+    const [player] = usePlayer();
+    const [stage, setStage] = useStage(player);
+
+    console.log('re-render');
 
     return (
         <StyledTetrisWrapper>
             <StyledTetris>
-                <Stage stage={createStage()} />
+                <Stage stage={stage} />
                 <aside>
-                    <div>
-                    <Display text="Score" />
-                    <Display text="Rows" />
-                    <Display text="Level" />
-                    </div>
+                    {gameOver ? (
+                        <Display gameOver={gameOver} text="Game Over" />
+                    ) : (
+                        <div>
+                            <Display text="Score" />
+                            <Display text="Rows" />
+                            <Display text="Level" />
+                        </div>
+                    )}
                     <StartButton />
                 </aside>
             </StyledTetris>
