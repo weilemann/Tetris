3d567f0ad595a945ce58b688dce4580dd1b17596
Yuri Weilemann <yuri_weilemann@hotmail.com>
2021-01-09 16:00:02 -0300

    Music loop inserted

--- src/components/Tetris/index.js
+++ src/components/Tetris/index.js
@@ -26,6 +26,7 @@ const Tetris = () => {
 
     useEffect(() => {
         playing ? audio.play() : audio.pause();
+        audio.loop = true;
     }, [playing]);
 
     const movePlayer = (dir) => {
